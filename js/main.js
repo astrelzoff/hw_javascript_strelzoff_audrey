e09e599361a4d4bcaac4e6ec249c1b9f3839d67c
Audrey Strelzoff <astrelzoff@ufl.edu>
2017-09-17 12:41:48 -0500

    Add iceCream variable equal to array of favorites
    Create a variable iceCream, set to an array of 3 favorite ice cream
    strings, then pushed a new string to the array for a 4th flavor

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Create the variable called iceCream
+var iceCream;
+
+// Set the variable iceCream to an array containing my top 3 flavors of ice cream
+iceCream = [
+    'Chocolate Mint Chip',
+    'Cookies and Cream',
+    'Chocolate Lovers'
+];
+// Add 'Caramel Swirl' to the array
+iceCream.push('Caramel Swirl');
